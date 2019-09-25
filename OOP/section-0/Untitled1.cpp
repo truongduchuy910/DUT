f04a04f81421e59514d047d8d059a2ab56e78129
Trần Ngọc Huy <truongduchuy910@gmail.com>
2019-09-25 21:06:33 +0700

    add file

--- OOP/section-0/Untitled1.cpp
+++ OOP/section-0/Untitled1.cpp
@@ -1,11 +1,18 @@
-#include <iostream>
-using namespace std;
-int *nhan_da_thuc (int *P, int *Q);
-int main () {
-	int P = 3;
-	int Q = 4;
-	cout << *nhan_da_thuc(&P, &Q);
-}
-int *nhan_da_thuc (int* P, int* Q) {
-	return (P);
+#include <stdio.h>
+#include <stdlib.h>
+char string[256];
+
+struct profile{
+	char name[15] = "huy";
+	int age = 19;
+	char gender[10] = "male";
+};
+
+struct profile user;
+int main()
+{ 
+	printf("name: %s\nage: %d\ngender: %s\n", user.name, user.age, user.gender);
+	printf("Nhap cau tra loi ");
+	fgets(string, 256, stdin);
+	printf("ban da tra loi: %s", string);
 }
--- OOP/section-0/test.cpp
+++ OOP/section-0/test.cpp
@@ -1,20 +1,4 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int main() {
-	int a, b, min;
-	cout << "Nhap a va b:";
-	cin >> a >> b;
-	min = a > b ? b : a;
-	int i;
-	for (i = 2; i < min; i++){
-		if ( (a % i ) == 0 && (b % i) == 0)  {
-			break;
-		}
-	}
-	if (i == min) {
-		cout << "Khong co mau chung nho nhat";
-	} else {
-		cout << "Mau chung nho nhat la: " << i;
-	}
-	return 0;
-}\
+    printf("hello world!");
+}
