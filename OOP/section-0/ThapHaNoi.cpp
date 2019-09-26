3a9c806fd28036310127954f57fe880c81a55bb3
truongduchuy910 <truongduchuy910@gmail.com>
2019-09-26 08:36:09 +0700

    docs

--- OOP/section-0/ThapHaNoi.cpp
+++ OOP/section-0/ThapHaNoi.cpp
@@ -1,7 +1,18 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-void tower(int n, char A, char B, char C) {
-    if(n == 1) {
-        cout << A << "->" << C << endl;
-    }
+void tower(int n, char a[], char b[], char c[]) {
+	if (n == 1) {
+		cout << a << "->" << c << endl;
+	} else {
+		tower(n - 1, a, c, b);
+		tower(1, a, b, c);
+		tower(n - 1, b, a, c);
+	}
+}
+int main() {
+	char a[] = "A";
+	char b[] = "B";
+	char c[] = "C";
+	tower(3, a, b, c);
+	return 0;
 }
