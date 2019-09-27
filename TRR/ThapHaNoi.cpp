50fbdc7d9d4ff90e7c43304281abfec778f9232b
truongduchuy910 <truongduchuy910@gmail.com>
2019-09-27 21:55:38 +0700

    thap ha noi, toan roi rac

--- TRR/ThapHaNoi.cpp
+++ TRR/ThapHaNoi.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
-void move(int disk, char* A, char* B, char* C) {
-	if (disk == 1) {
+void move(int n, char A, char B, char C) {
+	if (n == 1) {
 		cout << A << " to " << C << endl;
 	} else {
+		move(n - 1, A, C, B);
+		move(1, A, B, C);
+		move(n -1, B, A, C);
 	}
 }
 int main() {
+	char A = 'A';
+	char B = 'B';
+	char C = 'C';
+	cout << "Input number of disks in collum A: ";
+	int n;
+	cin >> n;
+	move(n, A, B, C);
 }	
