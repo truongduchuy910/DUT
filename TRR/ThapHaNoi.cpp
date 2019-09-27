8f88b44db43d7fea8d251df37161855a0248d33b
truongduchuy910 <truongduchuy910@gmail.com>
2019-09-27 21:35:43 +0700

    bai tap co hanh

---
+++ TRR/ThapHaNoi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void move(int disk, char* A, char* B, char* C) {
+	if (disk == 1) {
+		cout << A << " to " << C << endl;
+	} else {
+	}
+}
+int main() {
+}	
