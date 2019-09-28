9d0242fcbe8b69d8b831f7d5ec97a2c46a71f341
truongduchuy910 <truongduchuy910@gmail.com>
2019-09-28 19:48:27 +0700

    bai toan 8 hau, toan roi rac

--- OOP/section-0/DoiCoSo.cpp
+++ OOP/section-0/DoiCoSo.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 using namespace std;
-void doiCoSo(int n, int coSo) {
-    if (n == 0) {
-        cout << 0;
-    } else {
-        doiCoSo(n/coSo, coSo);
-        cout << n%coSo;
+char format[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+void doiCoSo(int n, int coSo)
+{
+    if (n == 0)
+    {
+    }
+    else
+    {
+        doiCoSo(n / coSo, coSo);
+        cout << format[n % coSo];
     }
 }
-int main() {
-    doiCoSo(4, 2);
+int main()
+{
+    doiCoSo(13233, 16);
+    cout << endl;
 }
--- TRR/XepHau.cpp
+++ TRR/XepHau.cpp
@@ -1,19 +1,73 @@
 #include <iostream>
 using namespace std;
-void tryCol(int i, int n, bool* col, bool*diaX, bool* diaY) { 
+int col[30], row[30], diaX[30], diaY[30];
+int n;
+void displayChessboard()
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (row[i] == j)
+			{
+				cout << " [o] ";
+			}
+			else
+			{
+				cout << " [ ] ";
+			}
+		}
+		cout << endl;
+	}
+	cout << "--------------------" << endl;
 };
-int main() {
+void putQueen(int i, int j)
+{
+	row[i] = j;
+	col[j] = false;
+	diaX[i + j] = false;
+	diaY[i - j + n - 1] = false;
+}
+void removeQueen(int i, int j)
+{
+	col[j] = true;
+	diaX[i + j] = true;
+	diaY[i - j + n - 1] = true;
+}
+bool validPostion(int i, int j)
+{
+	return (col[j] && diaX[i + j] && diaY[i - j + n - 1]);
+}
+void tryRow(int i)
+{
+	for (int j = 0; j < n; j++)
+		if (validPostion(i, j))
+		{
+			putQueen(i, j);
+
+			if (i == n - 1)
+			{
+				displayChessboard();
+			}
+			else
+			{
+				tryRow(i + 1);
+			}
+
+			removeQueen(i, j);
+		}
+};
+
+int main()
+{
 	cout << "Input size of chessboard (int): ";
-	int n;
 	cin >> n;
-	bool col[n];
-	bool diaX[2*n], diaY[2*n]; 
-	for (int i = 0; i < n; i++) {
+
+	for (int i = 0; i < 30; i++)
+	{
 		col[i] = true;
-	};
-	for (int i = 0; i < 2*n; i++) {
 		diaX[i] = true;
 		diaY[i] = true;
-	}; 
-	tryCol(1, n, col, diaX, diaY);
+	}
+	tryRow(0);
 }
