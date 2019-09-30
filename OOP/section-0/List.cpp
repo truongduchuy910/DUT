c8945ae6bd3c4a124eac1c4e7b0de721c2735492
truongduchuy910 <truongduchuy910@gmail.com>
2019-09-30 14:10:01 +0700

    do an lap trinh

---
+++ OOP/section-0/List.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+struct Data {
+	//non-pointer please
+};
+class List {
+	private:
+		Data data;
+	public: 
+		append(Data data);
+		push(Data data);
+		pop();
+		extend(List list);
+		insert(int index, Data data);
+		remove(int index);
+		index(int i);
+		int cout();
+		sort();
+		reverse();
+		List copy(int from, int to);
+}
+
+int main() {
+	return 0;
+}
