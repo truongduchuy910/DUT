5103e53d9891702da8a41984e00e42bb931d94c4
truongduchuy910 <truongduchuy910@gmail.com>
2019-09-30 14:04:24 +0700

    do an lap trinh

---
+++ DALT/views/home.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int home() {
+    cout << "1." << endl;
+    cout << "2." << endl;
+    cout << "3." << endl;
+    cout << "4." << endl;
+    cout << "5." << endl;
+    cout << "6." << endl;
+    cout << "7." << endl;
+    cout << "8." << endl;
+    cout << "Choose your option: (int) [1-8]: ";
+    int n;
+    cin >> n;
+    return n;
+}
