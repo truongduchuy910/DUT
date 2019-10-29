66cfed798056030f607446a3d336b9d0ac5879f6
truongduchuy910 <truongduchuy910@gmail.com>
2019-10-29 20:06:24 +0700

    DALT - danh sach lien ket

---
+++ DALT/table.cpp
@@ -0,0 +1,16 @@
+ostream &operator<<(ostream &ostream, Menu &menu)
+{
+    return ostream;
+};
+ostream &operator<<(ostream &ostream, Desk &desk)
+{
+    return ostream;
+};
+ostream &operator<<(ostream &ostream, Order &order)
+{
+    return ostream;
+};
+ostream &operator<<(ostream &ostream, Bill &bill)
+{
+    return ostream;
+};
