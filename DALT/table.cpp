ea58e459379ac7198ab6058ae8502a15df1b2509
truongduchuy910 <truongduchuy910@gmail.com>
2019-11-25 21:23:14 +0700

    OOP

--- DALT/table.cpp
+++ DALT/table.cpp
@@ -1,44 +1,52 @@
-Menu::Menu(string name, int foodId, int cost): name(name), foodId(foodId), cost(cost){};
+#include "table.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Menu::Menu(string name, int foodId, int cost) : name(name), foodId(foodId), cost(cost){};
 Menu::~Menu(){};
 istream &operator>>(istream &istream, Menu &menu)
 {
-	cout <<"Nhap ten mon: ";
+	cout << "Nhap ten mon: ";
 	istream >> menu.name;
-	cout <<"Ma mon: ";
-	istream >> menu.foodId; 
-	cout <<endl <<"Cost: ";
+	cout << "Ma mon: ";
+	istream >> menu.foodId;
+	cout << endl
+		 << "Cost: ";
 	istream >> menu.cost;
 };
 ostream &operator<<(ostream &ostream, Menu &menu)
 {
-	ostream <<menu.name <<" : " <<menu.cost;
-    return ostream;
+	ostream << menu.name << " : " << menu.cost;
+	return ostream;
 };
-Desk::Desk(int deskId, string status): deskId(deskId), status(status){};
+Desk::Desk(int deskId, string status) : deskId(deskId), status(status){};
 Desk::~Desk(){};
 istream &operator>>(istream &istream, Desk &desk)
 {
-	cout <<"DeskID: ";
-	istream >>desk.deskId;
-	cout <<endl <<"desk status: ";
+	cout << "DeskID: ";
+	istream >> desk.deskId;
+	cout << endl
+		 << "desk status: ";
 	istream >> desk.status;
 };
 ostream &operator<<(ostream &ostream, Desk &desk)
 {
-	ostream<<desk.deskId <<": "<<desk.status;
-    return ostream;
+	ostream << desk.deskId << ": " << desk.status;
+	return ostream;
 };
-Order::Order(int stt, int number, int billId): stt(stt), number(number), billId(billId){};
+Order::Order(int stt, int number, int billId) : stt(stt), number(number), billId(billId){};
 Order::~Order(){};
 ostream &operator<<(ostream &ostream, Order &order)
 {
-    ostream <<order.stt <<" || "<<order.menu->foodId <<" || " << order.number << "||" <<order.desk->deskId <<"||"<<order.billId;
-    return ostream;
+	ostream << order.stt << " || " << order.menu->foodId << " || " << order.number << "||" << order.desk->deskId << "||" << order.billId;
+	return ostream;
 };
-Bill::Bill(int total): total(total){};
+Bill::Bill(int total) : total(total){};
 Bill::~Bill(){};
 ostream &operator<<(ostream &ostream, Bill &bill)
 {
-	ostream <<"BillId: "<< bill.order->billId << "||" <<"Cost Total: " <<bill.total<<endl;
-    return ostream;
+	ostream << "BillId: " << bill.order->billId << "||"
+			<< "Cost Total: " << bill.total << endl;
+	return ostream;
 };
--- DALT/table.h
+++ DALT/table.h
@@ -1,3 +1,9 @@
+#ifndef table_h
+#define table_h
+
+#include <iostream>
+#include <string>
+using namespace std;
 
 class Menu;
 class Desk;
@@ -11,18 +17,18 @@ public:
     string name;
     int foodId;
     int cost;
-	Menu(string ="", int =0, int =0);
-	~Menu();
+    Menu(string = "", int = 0, int = 0);
+    ~Menu();
     friend ostream &operator<<(ostream &, Menu &);
     friend istream &operator>>(istream &, Menu &);
 };
 class Desk
 {
 public:
-	int deskId;
+    int deskId;
     string status;
-    Desk(int = 0, string ="");
-	~Desk();
+    Desk(int = 0, string = "");
+    ~Desk();
     friend ostream &operator<<(ostream &, Desk &);
     friend istream &operator>>(istream &, Desk &);
 };
@@ -33,25 +39,26 @@ private:
     Desk *desk;
     Bill *bill;
     //----------
-    int stt;              //So thu tu
-    int number;           //So luong
+    int stt;    //So thu tu
+    int number; //So luong
 
 public:
-	int billId;
-	Order(int =0, int =0, int =0);
-	~Order();
+    int billId;
+    Order(int = 0, int = 0, int = 0);
+    ~Order();
     friend ostream &operator<<(ostream &, Order &);
     friend ostream &operator>>(istream &, Order &);
 };
 class Bill
 {
-	
+
 private:
-	Order *order;
+    Order *order;
     int total;
+
 public:
-	Bill(int = 0);
-	~Bill();
+    Bill(int = 0);
+    ~Bill();
     friend ostream &operator<<(ostream &, Bill &);
 };
 class Table
@@ -62,3 +69,4 @@ public:
     Order order;
     Bill bill;
 };
+#endif
