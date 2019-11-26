81042fa7ad22b94af6ec793afe42069ff60f7036
truongduchuy910 <truongduchuy910@gmail.com>
2019-11-26 14:20:12 +0700

    chatbot

--- DALT/main.cpp
+++ DALT/main.cpp
@@ -1,17 +1,17 @@
 #include "table.h"
 Table table;
-// #include "views.h"
-// Views views;
-// #include "controller.h"
-// Controllers controllers;
-//#include "table.cpp"
-// #include "views.cpp"
-// #include "controller.cpp"
-// #include "test.cpp"
-// #include "list.h"
+#include "views.h"
+Views views;
+#include "controller.h"
+Controllers controllers;
+#include "table.cpp"
+#include "views.cpp"
+#include "controller.cpp"
+#include "test.cpp"
+#include "list.h"
 int main()
 {
-    //  test();
-    //  controllers.home();
+    test();
+    controllers.home();
     return 0;
 }
