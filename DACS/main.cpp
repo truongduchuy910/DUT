9d8fd35be3e0e5d72b9922db944910e3285adf0a
truongduchuy910 <truongduchuy910@gmail.com>
2019-12-19 23:46:27 +0700

    qdc

--- DACS/main.cpp
+++ DACS/main.cpp
@@ -3,81 +3,84 @@
 #include <iomanip>
 using namespace std;
 
-template <typename Data>
-class List;
-template <typename Data>
+class Array;
 class Node;
 
-
-
-template <typename Data>
 class Node
 {
-	friend class List<Data>;
+	friend class Array;
 
 public:
-	double(Data);
-	Data getData();
+	Node(double);
+	double &getData();
 
 private:
-	Data data;
-	double* next;
+	double data;
+	Node *next;
 };
 
-template <typename Data>
-class List
-{
-public:
-	int length;
-	List();
-	~List();
-	int insert(const Data&);
-	int findAndRemove(const Data&);
-	int print();
-	int empty();
-	Data operator[](int);
-
-private:
-	double<Data>* start;
-	bool isEmpty();
-};
-template <typename Data>
-double<Data>::double(Data dataIn)
+Node::Node(double dataIn)
 {
 	data = dataIn;
 	next = NULL;
 }
 
-template <typename Data>
-Data double<Data>::getData()
+double &Node::getData()
 {
 	return data;
 }
 
-template <typename Data>
-List<Data>::List()
+class Array
+{
+public:
+	int length;
+	Array();
+	Array(int);
+	~Array();
+	int insertLast(const double &);
+	double &operator[](int);
+	friend Array operator*(Array &, Array &);
+	friend ostream &operator<<(ostream &, Array &);
+	friend istream &operator>>(istream &, Array &);
+
+private:
+	Node *start;
+	Node *end;
+	bool isEmpty();
+};
+Array::Array()
 {
 	start = NULL;
+	end = NULL;
+	length = 0;
+}
+Array::Array(int size)
+{
+	length = 0;
+	start = NULL;
+	end = NULL;
+	while (size)
+	{
+		this->insertLast(0);
+		size--;
+	}
 }
 
-template <typename Data>
-List<Data>::~List()
+Array::~Array()
 {
 	if (!isEmpty())
 	{
-		double<Data>* currentdouble = start;
-		double<Data>* temp;
-
-		while (currentdouble != NULL)
+		Node *currentNode = start;
+		Node *temp;
+		while (currentNode != NULL)
 		{
-			temp = currentdouble;
-			currentdouble = currentdouble->next;
+			temp = currentNode;
+			currentNode = currentNode->next;
 		}
 	}
 }
 
-template <typename Data>
-bool List<Data>::isEmpty()
+bool Array::isEmpty()
 {
 	if (start == NULL)
 		return 1;
@@ -85,286 +88,242 @@ bool List<Data>::isEmpty()
 		return 0;
 }
 
-template <typename Data>
-int List<Data>::insert(const Data& dataIn)
+int Array::insertLast(const double &dataIn)
 {
-	cout << "LIST [INSERT] ";
+	Node *newNode = new Node(dataIn);
+
 	if (isEmpty())
 	{
-		double<Data>* newdouble = new double<Data>(dataIn);
-		start = newdouble;
+		start = newNode;
+		end = newNode;
 		this->length++;
-		cout << "Success" << endl;
 		return true;
 	}
 	else
 	{
-		double<Data>* newdouble = new double<Data>(dataIn);
-		newdouble->next = start;
-		start = newdouble;
+		end->next = newNode;
+		end = newNode;
 		this->length++;
-		cout << "Success" << endl;
 		return true;
 	}
-	cout << "Fail" << endl;
 	return false;
 }
 
-template <typename Data>
-int List<Data>::findAndRemove(const Data& dataIn)
+double &Array::operator[](int i)
 {
-	cout << "LIST [DELETE] ";
-	double<Data>* temp = this->start;
-	if (start != NULL && (start->data == dataIn)) {
-		start = start->next;
-		this->length--;
-		cout << "Success" << endl;
-		return true;
-
-	}
-	else {
-		while ((temp->next != NULL) && !(temp->next->data == dataIn)) {
-			temp = temp->next;
-		}
-		if (temp->next != NULL) {
-			this->length--;
-			temp->next = temp->next->next;
-			cout << "Success" << endl;
-			return true;
-		}
-
-	}
-	cout << "Fail" << endl;
-	return true;
-}
-
-template <typename Data>
-int List<Data>::print()
-{
-	if (isEmpty())
+	Node *temp = this->start;
+	while (i && temp != NULL)
 	{
-		cout << "The list is empty" << endl;
-	}
-	else
-	{
-		double<Data>* currentdouble = start;
-		while (currentdouble != NULL) //prints until the end of the list is reached
-		{
-			if (currentdouble != NULL) {
-				cout << currentNode->data << endl;
-
-			}
-			currentNode = currentNode->next; //moves to next node in list
-		}
-	}
-	return true;
-}
-template <typename Data>
-int List<Data>::empty() {
-	this->start = NULL;
-	return true;
-}
-template <typename Data>
-Data List<Data>::operator[](int i) {
-	Node<Data>* temp = this->start;
-	while (i && temp != NULL) {
 		temp = temp->next;
 		i--;
-	}
-	return temp->data;
-};
-
-class Array {
-private:
-	Array* next;
-	double value;
-public:
-	int length;
-	Array(int);
-	~Array();
-	double insertLast(double);
-	double& operator[](int);
-	friend Array operator*(Array&, Array&);
-	friend ostream& operator<<(ostream&, Array&);
-	friend istream& operator>>(istream&, Array&);
-};
-
-Array::Array(int size = 1) {
-	length = 1;
-	this->next = NULL;
-	this->value = 0;
-	while (--size) {
-		this->insertLast(0);
-	}
-}
-
-Array::~Array() {}
-
-double Array::insertLast(double value) {
-	Array* lastNode = this;
-	length++;
-	while (lastNode->next != NULL) {
-		lastNode = lastNode->next;
-	}
-	Array* newLastNode = new Array;
-	newLastNode->value = value;
-	newLastNode->next = NULL;
-	lastNode->next = newLastNode;
-	return value;
-}
-
-double& Array::operator[](int i) {
-	Array* node = this;
-	int index = 0;
-	while (index != i && node != NULL) {
-		index++;
-		node = node->next;
 	};
-	return node->value;
-}
+	return temp->getData();
+};
 
-ostream& operator<<(ostream& ostream, Array& A) {
-	for (int i = 0; i < A.length; i++) {
-		ostream << setw(8) << A[i];
-	}
-	return ostream;
-}
-
-istream& operator>>(istream& istream, Array& A) {
-	for (int i = 0; i < A.length; i++) {
-		cout << "He so cua x^" << i << ": ";
-		istream >> A[i];
-	}
-	return istream;
-}
-
-Array operator*(Array& A, Array& B) {
+Array operator*(Array &A, Array &B)
+{
 	Array result(A.length + B.length - 1);
-	for (int i = 0; i < A.length; i++) {
-		for (int j = 0; j < B.length; j++) {
+	for (int i = 0; i < A.length; i++)
+	{
+		for (int j = 0; j < B.length; j++)
+		{
 			int k = i + j;
 			result[k] += A[i] * B[j];
 		}
 	}
 	return result;
 }
+ostream &operator<<(ostream &ostream, Array &A)
+{
+	for (int i = 0; i < A.length; i++)
+	{
+		ostream << setw(8) << A[i];
+	}
+	return ostream;
+}
 
-int readData(Array& A, const char* path) {
+istream &operator>>(istream &istream, Array &A)
+{
+	for (int i = 0; i < A.length; i++)
+	{
+		cout << "He so cua x^" << i << ": ";
+		istream >> A[i];
+	}
+	return istream;
+}
+
+int readData(Array &A, const char *path)
+{
 	ifstream file;
 	file.open(path);
 	if (file.fail())
 	{
+		cout << "Doc file " << path
+			 << " that bai!" << endl;
 		return 0; //fail
 	}
 	double value;
 	int index = 0;
-	while (file >> value) {
-		if (index < A.length) {
+	while (file >> value)
+	{
+		if (index < A.length - 1)
+		{
 			A[index] = value;
 		}
-		else {
+		else
+		{
 			A.insertLast(value);
 		}
 		index++;
 	};
 	file.close();
+	cout << "Doc file " << path << " thanh cong!" << endl;
 	return 1; //success
 }
 
-int writeData(Array& A, const char* path) {
+int writeData(Array &A, const char *path)
+{
 	ofstream file(path);
 	if (file.fail())
 	{
+		cout << "ghi file " << path << " that bai!";
 		return 0; //fail
 	}
-	for (int i = 0; i < A.length; i++) {
+	for (int i = 0; i < A.length; i++)
+	{
 		file << setw(8) << A[i];
 	}
 	file.close();
+	cout << "Ghi file " << path << " thanh cong!" << endl;
 	return 1; //success
 }
 
-int controller() {
+int controller()
+{
 	int option;
-	do {
-		cout << endl;
-		cout << "------ MENU DIEU KHIEN ----- " << endl;
-		cout << "0. Thoat chuong trinh" << endl;
-		cout << "1. Doc du lieu tu file" << endl;
-		cout << "2. Nhap du lieu tu ban phim" << endl;
-		cout << "Nhap lua chon: ";
+	do
+	{
+		cout << endl
+			 << "------ MENU DIEU KHIEN ----- " << endl
+			 << "0. Thoat chuong trinh" << endl
+			 << "1. Doc du lieu tu file" << endl
+			 << "2. Nhap du lieu tu ban phim" << endl
+			 << "Nhap lua chon: ";
 		cin >> option;
 	} while (option < 0 || option > 2);
 	return option;
 }
 
-int exit() {
+int exit()
+{
 	int option;
-	do {
-		cout << endl;
-		cout << "Thoat chuong trinh? " << endl;
-		cout << "1. Tiep tuc chuong trinh" << endl;
-		cout << "0. Thoat chuong trinh" << endl;
-		cout << "Nhap lua chon: ";
+	do
+	{
+		cout << endl
+			 << "Thoat chuong trinh? " << endl
+			 << "1. Tiep tuc chuong trinh" << endl
+			 << "0. Thoat chuong trinh" << endl
+			 << "Nhap lua chon: ";
 		cin >> option;
 	} while (option < 0 || option > 1);
 	return option;
 }
-
-int main()
+class
 {
-	cout << endl << "-- CHUONG TRINH TINH TICH 2 DA THUC --" << endl << endl;
-	cout << "Ma nhom: 18N15 - N6" << endl;
-	cout << "Thanh vien trong nhom: " << endl;
-	cout << "1. Nguyen Kim Huy - MSSV: 102180207" << endl;
-	cout << "2. Tran Ngoc Huy  - MSSV: 102180208" << endl << endl;
-	do {
-		switch (controller()) {
+public:
+	void wellcome()
+	{
+		cout << "+-------------------------------------+" << endl
+			 << "| Chuong Trinh Tinh Tich Da Thuc      |" << endl
+			 << "+-------------------------------------+" << endl
+			 << "| Ma nhom: 18N15 - N6                 |" << endl
+			 << "| Thanh vien trong nhom:              |" << endl
+			 << "| 1. Nguyen Kim Huy - MSSV: 102180207 |" << endl
+			 << "| 2. Tran Ngoc Huy  - MSSV: 102180208 |" << endl
+			 << "+-------------------------------------+" << endl;
+	}
+	void home()
+	{
+		int option;
+		do
+		{
+			cout << "0. Thoat chuong trinh" << endl
+				 << "1. Doc du lieu tu file" << endl
+				 << "2. Nhap du lieu tu ban phim" << endl
+				 << "Nhap lua chon: ";
+
+			cin >> option;
+		} while (option < 0 || option > 2);
+		cout << "---------------------------------------" << endl;
+
+		switch (option)
+		{
 		case 1:
 		{
-			cout << "He so cua da thuc duoc nhap tu bac 0 den bac cao nhat cua da thuc do" << endl;
-			Array P, Q, R;
-			if (readData(P, "DATA1.txt") && readData(Q, "DATA2.txt")) {
-				cout << "Doc du lieu tu file DATA1.txt va DATA2.txt thanh cong" << endl;
-				R = P * Q;
-				if (writeData(R, "DATA3.txt")) {
-					cout << "Ghi du lieu ra file DATA3.txt thanh cong" << endl;
-					cout << "Ket qua:" << endl;
-					cout << "He so da thuc P(x) (file DATA1.txt): " << P << endl;
-					cout << "He so da thuc Q(x) (file DATA2.txt): " << Q << endl;
-					cout << "He so da thuc tich R(x) = P(x) * Q(x) (file DATA3.txt): " << endl << R << endl;
-				}
-				else cout << "Ghi du lieu ra file DATA3.txt that bai!" << endl;
-			}
-			else cout << "Doc du lieu tu file that bai!" << endl;
+			fileInput();
+			home();
 			break;
 		}
 		case 2:
 		{
-			cout << "Nhap bac cao nhat cua ham P(x) va Q(x)" << endl;
-			cout << "Nhap he so cua moi da thuc tu bac 0 den bac cao nhat cua da thuc do" << endl;
-			cout << "Vi du: P(x) = 2 + 3*x - 5.5*x^3 thi nhap bac la bac 3, he so lan luot la: 2 3 0 -5.5" << endl;
-			int bacP, bacQ, bacR;
-			cout << endl << "Nhap bac da thuc P(x): ";
-			cin >> bacP;
-			Array P(bacP + 1);
-			cout << "Nhap cac he so: " << endl;
-			cin >> P;
-			cout << endl << "Nhap bac da thuc Q(x): ";
-			cin >> bacQ;
-			Array Q(bacQ + 1);
-			cout << "Nhap cac he so: " << endl;
-			cin >> Q;
-			Array R(P.length + Q.length - 1);
-			R = P * Q;
-			cout << endl;
-			cout << "Ket qua: " << endl;
-			cout << "He so da thuc P(x): " << P << endl;
-			cout << "He so da thuc Q(x): " << Q << endl;
-			cout << "He so da thuc tich R(x) = P(x) * Q(x): " << endl << R << endl;
+			consoleInput();
+			home();
 			break;
 		}
 		}
-	} while (exit() == 1);
+	}
+	void fileInput()
+	{
+		cout << "He so cua duoc nhap theo so mu tu 0 den n" << endl;
+		Array P, Q, R;
+		bool noError = true;
+		noError *= readData(P, "DATA1.txt");
+		noError *= readData(Q, "DATA2.txt");
+		R = P * Q;
+		noError *= writeData(R, "DATA3.txt");
+		if (noError)
+			cout << "He so P(x): " << endl
+				 << P << endl
+				 << "He so Q(x): " << endl
+				 << Q << endl
+				 << "He so tich R(x) = P(x) * Q(x): " << endl
+				 << R << endl
+				 << "---------------------------------------" << endl;
+	}
+	void consoleInput()
+	{
+		cout << "Nhap bac P(x) va Q(x)" << endl
+			 << "Nhap he so theo so mu tu 0 den n " << endl
+			 << "Vi du: P(x) = 2 + 3*x - 5.5*x^3 | bac: 3, he so: 2 3 0 -5.5" << endl;
+		int bac;
+		cout << "Nhap bac P(x): " << endl;
+		cin >> bac;
+		Array P(bac + 1);
+		cout << "Nhap cac he so: " << endl;
+		cin >> P;
+		cout << "Nhap bac Q(x): " << endl;
+		cin >> bac;
+		Array Q(bac + 1);
+		cout << "Nhap cac he so: " << endl;
+		cin >> Q;
+		Array R(P.length + Q.length - 1);
+		R = P * Q;
+		cout << endl
+			 << "Ket qua: " << endl
+			 << "He so P(x): " << endl
+			 << P << endl
+			 << "He so Q(x): " << endl
+			 << Q << endl
+			 << "He so tich R(x) = P(x) * Q(x):" << endl
+			 << R << endl
+			 << "---------------------------------------" << endl;
+		;
+	}
+} controllers;
+int main()
+{
+
+	controllers.wellcome();
+	controllers.home();
 	return 0;
 }
