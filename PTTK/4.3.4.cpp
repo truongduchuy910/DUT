0f9513aededffcb07c4e270523c8e9c897eee2e9
truongduchuy910 <truongduchuy910@gmail.com>
2019-12-28 10:41:17 +0700

    thapHaNoi

--- PTTK/4.3.4.cpp
+++ PTTK/4.3.4.cpp
@@ -6,9 +6,22 @@ bool move(int disk, int i, int j) {
         cout << i << " to " << j << endl;
         move(disk - 1, 6 - i - j, j);
 }
+bool moveOrther(int disk, int a, int b, int c) {
+	if (disk == 1) {
+		cout << a << " to " << c << endl;
+		return false;
+	}
+	moveOrther(disk - 1, a, c, b);
+	moveOrther(1, a, b, c);
+	moveOrther(disk - 1, b, a, c);
+	return true;
+}
 int main() {
 	cout << "n: ";
 	int n;
 	cin >> n;
+	cout << "solve with move" << endl;
 	move(n, 1, 3);
+	cout << "solve with moveOrther" << endl;
+	moveOrther(n, 1, 2, 3);
 }
