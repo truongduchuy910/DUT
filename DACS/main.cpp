ea540cc4f62b770ac0ae20ba83322d63133ba8f5
truongduchuy910 <truongduchuy910@gmail.com>
2019-12-30 23:56:26 +0700

    done

--- DACS/main.cpp
+++ DACS/main.cpp
@@ -240,6 +240,7 @@ ostream &operator<<(ostream &os, const Polynomial &m)
 	{
 		os << setw(5) << m[i];
 	}
+	cout << endl;
 	return os;
 }
 Polynomial &Polynomial::operator=(const Polynomial &a)
@@ -284,118 +285,152 @@ Polynomial &Polynomial::operator=(const Polynomial &a)
 // 	return 1; //success
 // }
 
-// class
-// {
-// public:
-// 	void welcome()
-// 	{
-// 		cout << "+-------------------------------------+" << endl
-// 			 << "| Chuong Trinh Tinh Tich Da Thuc      |" << endl
-// 			 << "+-------------------------------------+" << endl
-// 			 << "| Ma nhom: 18N15 - N6                 |" << endl
-// 			 << "| Thanh vien trong nhom:              |" << endl
-// 			 << "| 1. Nguyen Kim Huy - MSSV: 102180207 |" << endl
-// 			 << "| 2. Tran Ngoc Huy  - MSSV: 102180208 |" << endl
-// 			 << "+-------------------------------------+" << endl;
-// 	}
-// 	void home()
-// 	{
-// 		int option;
-// 		do
-// 		{
-// 			cout << "0. Thoat chuong trinh" << endl
-// 				 << "1. Doc du lieu tu file" << endl
-// 				 << "2. Nhap du lieu tu ban phim" << endl
-// 				 << "Nhap lua chon: ";
+Polynomial A;
+Polynomial B;
+Polynomial C;
+class
+{
+public:
+	void views()
+	{
+		cout << "Nhap da thuc A" << endl;
+		cin >> A;
+		cout << "Nhap da thuc B" << endl;
+		cin >> B;
+	}
+	void options()
+	{
+		cout << "Chon toan tu" << endl
+			 << "0. tro ve" << endl
+			 << "1. +" << endl
+			 << "2. -" << endl
+			 << "3. *" << endl
+			 << "4. /" << endl
+			 << "Chon so ban muon [1-4]: ";
+		int choose;
+		cin >> choose;
+		cout << "ket qua: " << endl;
+		switch (choose)
+		{
+		case 0:
+			break;
+		case 1:
+			cout << A + B;
+			options();
+			break;
+		case 2:
+			cout << A - B;
+			options();
+			break;
+		case 3:
+			cout << A * B;
+			options();
+			break;
+		case 4:
+			cout << A / B;
+			options();
+			break;
 
-// 			cin >> option;
-// 		} while (option < 0 || option > 2);
-// 		cout << "---------------------------------------" << endl;
+		default:
+			break;
+		}
+	}
+} consoleInput;
+class
+{
+public:
+	void views()
+	{
+		cout << "+-------------------------------------+" << endl
+			 << "| Chuong Trinh Tinh Tich Da Thuc      |" << endl
+			 << "+-------------------------------------+" << endl
+			 << "| Ma nhom: 18N15 - N6                 |" << endl
+			 << "| Thanh vien trong nhom:              |" << endl
+			 << "| 1. Nguyen Kim Huy - MSSV: 102180207 |" << endl
+			 << "| 2. Tran Ngoc Huy  - MSSV: 102180208 |" << endl
+			 << "+-------------------------------------+" << endl;
+	}
+	void options()
+	{
+		int choose;
+		do
+		{
+			cout << "0. Thoat chuong trinh" << endl
+				 << "1. Doc du lieu tu file" << endl
+				 << "2. Nhap du lieu tu ban phim" << endl
+				 << "Nhap lua chon: ";
 
-// 		switch (option)
-// 		{
-// 		case 1:
-// 		{
-// 			fileInput();
-// 			home();
-// 			break;
-// 		}
-// 		case 2:
-// 		{
-// 			consoleInput();
-// 			home();
-// 			break;
-// 		}
-// 		}
-// 	}
-// 	void fileInput()
-// 	{
-// 		cout << "He so cua duoc nhap theo so mu tu 0 den n" << endl;
-// 		Polynomial P, Q, R;
-// 		bool noError = true;
-// 		noError *= readData(P, "DATA1.txt");
-// 		noError *= readData(Q, "DATA2.txt");
-// 		R = P * Q;
-// 		noError *= writeData(R, "DATA3.txt");
-// 		if (noError)
-// 			cout << "He so P(x): " << endl
-// 				 << P << endl
-// 				 << "He so Q(x): " << endl
-// 				 << Q << endl
-// 				 << "He so tich R(x) = P(x) * Q(x): " << endl
-// 				 << R << endl
-// 				 << "---------------------------------------" << endl;
-// 	}
-// 	void consoleInput()
-// 	{
-// 		cout << "Nhap size P(x) va Q(x)" << endl
-// 			 << "Nhap he so theo so mu tu 0 den n " << endl
-// 			 << "Vi du: P(x) = 2 + 3*x - 5.5*x^3 | size: 3, he so: 2 3 0 -5.5" << endl;
-// 		int size;
-// 		cout << "Nhap size P(x): " << endl;
-// 		cin >> size;
-// 		Polynomial P(size + 1);
-// 		cout << "Nhap cac he so: " << endl;
-// 		cin >> P;
-// 		cout << "Nhap size Q(x): " << endl;
-// 		cin >> size;
-// 		Polynomial Q(size + 1);
-// 		cout << "Nhap cac he so: " << endl;
-// 		cin >> Q;
-// 		Polynomial R(P.size + Q.size - 1);
-// 		R = P * Q;
-// 		cout << endl
-// 			 << "Ket qua: " << endl
-// 			 << "He so P(x): " << endl
-// 			 << P << endl
-// 			 << "He so Q(x): " << endl
-// 			 << Q << endl
-// 			 << "He so tich R(x) = P(x) * Q(x):" << endl
-// 			 << R << endl
-// 			 << "---------------------------------------" << endl;
-// 	}
-// } controllers;
+			cin >> choose;
+		} while (choose < 0 || choose > 2);
+		cout << "---------------------------------------" << endl;
+
+		switch (choose)
+		{
+		case 1:
+		{
+			//	fileInput();
+			options();
+			break;
+		}
+		case 2:
+		{
+			consoleInput.views();
+			consoleInput.options();
+			this->options();
+			break;
+		}
+		}
+	}
+	// void fileInput()
+	// {
+	// 	cout << "He so cua duoc nhap theo so mu tu 0 den n" << endl;
+	// 	Polynomial P, Q, R;
+	// 	bool noError = true;
+	// 	noError *= readData(P, "DATA1.txt");
+	// 	noError *= readData(Q, "DATA2.txt");
+	// 	R = P * Q;
+	// 	noError *= writeData(R, "DATA3.txt");
+	// 	if (noError)
+	// 		cout << "He so P(x): " << endl
+	// 			 << P << endl
+	// 			 << "He so Q(x): " << endl
+	// 			 << Q << endl
+	// 			 << "He so tich R(x) = P(x) * Q(x): " << endl
+	// 			 << R << endl
+	// 			 << "---------------------------------------" << endl;
+	// }
+	// void consoleInput()
+	// {
+	// 	cout << "Nhap size P(x) va Q(x)" << endl
+	// 		 << "Nhap he so theo so mu tu 0 den n " << endl
+	// 		 << "Vi du: P(x) = 2 + 3*x - 5.5*x^3 | size: 3, he so: 2 3 0 -5.5" << endl;
+	// 	int size;
+	// 	cout << "Nhap size P(x): " << endl;
+	// 	cin >> size;
+	// 	Polynomial P(size + 1);
+	// 	cout << "Nhap cac he so: " << endl;
+	// 	cin >> P;
+	// 	cout << "Nhap size Q(x): " << endl;
+	// 	cin >> size;
+	// 	Polynomial Q(size + 1);
+	// 	cout << "Nhap cac he so: " << endl;
+	// 	cin >> Q;
+	// 	Polynomial R(P.size + Q.size - 1);
+	// 	R = P * Q;
+	// 	cout << endl
+	// 		 << "Ket qua: " << endl
+	// 		 << "He so P(x): " << endl
+	// 		 << P << endl
+	// 		 << "He so Q(x): " << endl
+	// 		 << Q << endl
+	// 		 << "He so tich R(x) = P(x) * Q(x):" << endl
+	// 		 << R << endl
+	// 		 << "---------------------------------------" << endl;
+	// }
+} home;
 int main()
 {
-	Polynomial A;
-	Polynomial B;
-	Polynomial C;
-	cin >> A >> B;
-	cout << "A" << endl;
-	cout << A << endl;
-	cout << "B" << endl;
-	cout << B << endl;
-	cout << "A + B" << endl;
-	C = A + B;
-	cout << C << endl;
-	cout << "A - B" << endl;
-	C = A - B;
-	cout << C << endl;
-	cout << "A * B" << endl;
-	C = A * B;
-	cout << C << endl;
-	cout << "A / B" << endl;
-	C = A / B;
-	cout << C << endl;
+	home.views();
+	home.options();
 	return 0;
 }
